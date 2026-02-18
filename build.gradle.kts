feffb845402d04d8f6a7f60c06f8437299713331
Mike Wiedenbauer <mike.wiedenbauer@unblu.com>
2026-02-18 20:43:35 +0100

    upgrade plugin versions (fix NoClassDefFound error) use await

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 plugins {
 	id("org.springframework.boot") version "2.4.1"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+	kotlin("jvm") version "1.5.30"
+	kotlin("plugin.spring") version "1.5.30"
 }
 
 group = "com.unblu.demo"
